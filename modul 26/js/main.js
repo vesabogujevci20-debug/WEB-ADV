758f896aa7a8a7b0a70b97f3d7071f48ccfee7a4
vesabogujevci20-debug <vesabogujevci20@gmail.com>
2026-05-06 19:17:40 +0200

    modul 26

---
+++ modul 26/js/main.js
@@ -0,0 +1,44 @@
+console.log('Hello World!');
+
+var x=5;
+
+var b=20+34;
+
+document.write(b);
+
+var a=99;
+a--;
+console.log(a);
+
+var y=20;
+var q=y**2;
+console.log(q);
+
+var z=12;
+z+=3;
+console.log(z);
+
+var r=5;
+var t=5;
+console.log(r==t);
+
+var u=5;
+var i=4;
+console.log(u!=i);
+
+var r=5;
+var t=5;
+console.log(r>=t);
+
+var u=4;
+var i="4";
+console.log(u===i);
+
+var emri="digital";
+var mbiemri="academy";
+var fullname=emri+" "+mbiemri;
+console.log(fullname);
+
+var school="digital academy";
+console.log(`hello from ${school}`);
+
