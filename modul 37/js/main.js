adb199c837b9e5dcbf4ebc1d6a7c30f1c379e16e
vesabogujevci20-debug <vesabogujevci20@gmail.com>
2026-06-24 19:03:05 +0200

    modul37

---
+++ modul 37/js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('#sort').sortable();
+
+    $('#tabs').tabs({
+        collapsible:true
+    });
+    $('#accordion').accordion({
+        collapsible:true
+
+    });
+
+
+});
--- modul36/js/main.js
+++ modul36/js/main.js
@@ -3,4 +3,5 @@ $(window).ready(function(){
     $('#test1').css('color', 'red');
     $('.test2').css('background-color','darkblue');
     $('#myform : input').css('border', '3px solid red');
+    $('div:visible').css('color', ' blue');
 });
